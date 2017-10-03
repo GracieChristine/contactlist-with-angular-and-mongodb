b1d5c23f1c9b025bd06bf95a5fa88de82611f3a9
GracieChristine <graciechristineli@gmail.com>
2017-10-03 09:55:16 -0600

    Directing express where to look.

--- server.js
+++ server.js
@@ -3,9 +3,8 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function(req, res) {
-  res.send("Hello world from server.js")
-});
+// Looking for static files inside /public
+app.use(express.static(__dirname + "/public"));
 
 app.listen(3000);
 console.log("Listening and spinning on port 3000");
