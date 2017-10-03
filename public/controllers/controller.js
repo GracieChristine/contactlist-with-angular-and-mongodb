4069095f3d047f69e1d6a7040fec8aa3bf801121
GracieChristine <graciechristineli@gmail.com>
2017-10-03 11:17:11 -0600

    Mocking up dummy data and finally make it show up in the table.

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -29,4 +29,25 @@
 const app = angular.module('myApp', []);
 app.controller('AppCtrl', function($scope) {
   console.log("I'm coming from the controller!!");
+    const person1 = {
+      name: 'Tim',
+      email: 'tim@email1.com',
+      number: '(111) 111-1111'
+    };
+
+    const person2 = {
+      name: 'Emily',
+      email: 'emily@email2.com',
+      number: '(222) 222-2222'
+    };
+
+    const person3 = {
+      name: 'John',
+      email: 'john@email3.com',
+      number: '(333) 333-3333'
+    };
+
+    const contactlist = [person1, person2, person3];
+
+    $scope.contactlist = contactlist;
 })
