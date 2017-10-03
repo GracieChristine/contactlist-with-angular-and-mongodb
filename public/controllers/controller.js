d525e446b0a3400e6710c30e0f1b8e3ab0c83895
GracieChristine <graciechristineli@gmail.com>
2017-10-03 11:27:25 -0600

    Receiving the dat I requested from server and controller.

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -4,27 +4,10 @@ const app = angular.module('myApp', []);
 app.controller('AppCtrl', function($scope, $http) {
   console.log("I'm coming from the controller!!");
 
-  $http.get('/contactlist')
-  
-    const person1 = {
-      name: 'Tim',
-      email: 'tim@email1.com',
-      number: '(111) 111-1111'
-    };
+  $http.get('/contactlist').success(function(response) {
+    console.log("Got the data requested");
+    $scope.contactlist = response;
+  });
 
-    const person2 = {
-      name: 'Emily',
-      email: 'emily@email2.com',
-      number: '(222) 222-2222'
-    };
 
-    const person3 = {
-      name: 'John',
-      email: 'john@email3.com',
-      number: '(333) 333-3333'
-    };
-
-    const contactlist = [person1, person2, person3];
-
-    $scope.contactlist = contactlist;
 })
--- server.js
+++ server.js
@@ -8,6 +8,28 @@ app.use(express.static(__dirname + "/public"));
 
 app.get('/contactlist', function(req, res) {
   console.log("Received a GET request");
+
+  const person1 = {
+    name: 'Tim',
+    email: 'tim@email1.com',
+    number: '(111) 111-1111'
+  };
+
+  const person2 = {
+    name: 'Emily',
+    email: 'emily@email2.com',
+    number: '(222) 222-2222'
+  };
+
+  const person3 = {
+    name: 'John',
+    email: 'john@email3.com',
+    number: '(333) 333-3333'
+  };
+
+  const contactlist = [person1, person2, person3];
+  // Response GET request in a JSON format
+  res.json(contactlist);
 });
 
 app.listen(3000);
